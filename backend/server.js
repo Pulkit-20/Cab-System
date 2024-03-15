bbe87737d903a2a6008ed9b9bca9203b823d3989
pulkit-20 <pulkit2469.be21@chitkara.edu.in>
2024-03-15 23:41:00 +0530

    build files

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const bookCabRoutes = require("./routes/bookCabRoutes");
 const cabRoutes = require("./routes/cabRoutes");
 const locationRoutes = require("./routes/locationRoutes");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -12,12 +13,25 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 app.use("/book", bookCabRoutes);
 app.use("/cab", cabRoutes);
 app.use("/location", locationRoutes);
+
+// Deployement
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+// ------
 
 const PORT = process.env.PORT || 5000;
 
